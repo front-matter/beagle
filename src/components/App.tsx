c94b0e746704dc17e1fa15a6e4f0dc4712fec514
Richard Hallett <rhallett@datacite.org>
2020-03-05 18:22:30 +0100

    Add react router
    * Basic routing screens
    * Switched basic test example
    * Removed template CSS

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from '../assets/logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>PID Services Registry</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/services">Services</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <Switch>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Services() {
+  return <h2>Services</h2>;
 }
 
 export default App;
