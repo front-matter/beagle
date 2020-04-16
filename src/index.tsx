93a30bd2112d8630291bb1fbdc84dfc7b515c5c7
Richard Hallett <rhallett@datacite.org>
2020-04-16 11:56:14 +0200

    Add sentry support

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import * as Sentry from '@sentry/browser';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+
+Sentry.init({dsn: "https://d9c7b78cf2b343198abc57028e432c70@o239790.ingest.sentry.io/5202325"});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
